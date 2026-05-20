7b7a907992808ed7a02b1e82f3dfde2687585de3
Rohit Kumar <Mr.developer2003@gmail.com>
2026-05-20 22:44:45 +0530

    Print all even number from 1 to N

--- _1.Simple-Number-Problems/_1.js
+++ _1.Simple-Number-Problems/_1.js
@@ -8,9 +8,22 @@
 
 // Print Numbers from 1 to N without changing the loop condition of above question 
 
-    const number= 100;
-        let cnt=1;
+    // const number= 100;
+    //     let cnt=1;
+    //         while(cnt<=number){
+    //             console.log(number-cnt+1);
+    //             cnt++;
+    //         }
+
+// Print all even number from 1 to N
+
+    const number = 100;
+        let cnt = 1;
             while(cnt<=number){
-                console.log(number-cnt+1);
+                if(cnt%2==0) {
+                    console.log(cnt,'Odd Number');
+                }else{
+                    console.log(cnt,'Even Number');
+                }
                 cnt++;
             }
