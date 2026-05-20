1d160914f0daf4dacc9a9ac3e8e609707b24364c
Rohit Kumar <Mr.developer2003@gmail.com>
2026-05-20 22:37:08 +0530

    Print Numbers from 1 to N without changing the loop condition of above question

--- _1.Simple-Number-Problems/_1.js
+++ _1.Simple-Number-Problems/_1.js
@@ -1,7 +1,16 @@
 // Print Number 1 to N 
-const number = 100;
-    let cnt = 1;
-        while(cnt<=number){
-            console.log(cnt);
-            cnt++;
-        }
+// const number = 100;
+//     let cnt = 1;
+//         while(cnt<=number){
+//             console.log(cnt);
+//             cnt++;
+//         }
+
+// Print Numbers from 1 to N without changing the loop condition of above question 
+
+    const number= 100;
+        let cnt=1;
+            while(cnt<=number){
+                console.log(number-cnt+1);
+                cnt++;
+            }
